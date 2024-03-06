a0b0a288c4be751670c5ddec1a6ad06cd610fab0
analogkeyboard <104320272+analogkeyboard@users.noreply.github.com>
2024-03-06 08:53:07 -0800

    Add files via upload

--- funcion2.c
+++ funcion2.c
@@ -1,16 +1,28 @@
 #include <STDIO.H>
+#include <STRING.H>
 #define NOMBRE "ORDENATAS, S.A."
 #define DIRECCION "Plaza del Byte 12"
 #define CIUDAD "Villabits, E- 60006"
 #define LIMITE 65
 
 char asteriscos();
+char espacios(int numero);
 int main(int argc, char const *argv[])
 {
+
+    int salta = (65 - strlen(DIRECCION)) / 2;
+
     asteriscos();
+
+    espacios(salta);
     printf("%s\n", NOMBRE);
+
+    espacios(salta);
     printf("%s\n", DIRECCION);
+
+    espacios(salta);
     printf("%s\n", CIUDAD);
+
     asteriscos();
     return 0;
 }
@@ -23,4 +35,13 @@ char asteriscos()
         putchar('*');
     }
     putchar('\n');
+}
+
+char espacios(int numero)
+{
+    for (int cont = 0; cont <= numero; cont++)
+    {
+        /* code */
+        putchar(' ');
+    }
 }
