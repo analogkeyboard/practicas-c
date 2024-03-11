f5d2c94e33329f3f7370746514db26806a1b0a3d
analogkeyboard <104320272+analogkeyboard@users.noreply.github.com>
2024-03-11 12:53:16 -0800

    Add files via upload

--- funciones_strcat_y_strncat.c
+++ funciones_strcat_y_strncat.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <string.h>
+#include <STDIO.H>
+#include <STRING.H>
 
 int main(int argc, char const *argv[])
 {
-    char cadena1[]="Feliz ";
-    char cadena2[]="Ano Nuevo ";
-    char cadena3[40]="";
+    char s1[20] = "Feliz ";
+    char s2[] = "Anio Nuevo ";
+    char s3[40] = "";
 
-    printf("Cadena1: %s\n", cadena1);
-    printf("Cadena2: %s\n", cadena2);
-    
+    printf("s1: %s\ns2: %s", s1, s2);
+
+    // concatena 2 cadenas
+    printf("\nstrcat(s1,s2) = %s", strcat(s1, s2));
+
+    // concatena los primeros 6 caracteres de s3 a s1
+
+    printf("\nstrncat(s3,s1,6) = %s", strncat(s3, s1, 6));
+
+    //concatena s1 con ese s3
+    printf("\nstrcat(s3,s1) = %s",strcat(s3,s1));
     return 0;
 }
